fe1facd561d3c5a8674ee381d86df0d6d625a72f
jadintasalsabilah <jadintasalsabilahh@gmail.com>
2017-12-29 08:58:16 +0700

    override

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -28,3 +28,28 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more AppWidget instances have been deleted
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Perform any action when an AppWidget for this provider is instantiated
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Perform any action when the last AppWidget instance for this provider is deleted
+    }
+
+}
